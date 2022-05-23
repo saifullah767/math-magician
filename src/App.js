7e57db83f451eb5c4e2f12958c1552bc3d33b243
skj99667788@gmail.com <skj99667788@gmail.com>
2022-05-23 23:34:01 +0500

    linters error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 
+          Edit
           <code>src/App.js</code>
-           and save to reload.
+          and save to reload.
         </p>
         <a
           className="App-link"
