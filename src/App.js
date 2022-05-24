a06f7652dd7cb01b31518ad53512029af069d631
skj99667788@gmail.com <skj99667788@gmail.com>
2022-05-25 03:30:01 +0500

    eslint error

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint linebreak-style: ['error', 'windows'] */
 import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,4 @@
+/* eslint linebreak-style: ["error", "windows"] */
 import React, { Component } from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
