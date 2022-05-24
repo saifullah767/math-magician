e2d3fd3b8cb4db80051c2b4cc5532bc80de8dda8
skj99667788@gmail.com <skj99667788@gmail.com>
2022-05-25 03:53:05 +0500

    eslint error

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Calculator from './components/Calculator';
 import './App.css';
 
 /* eslint linebreak-style: [2, "windows"] */
+
 // eslint-disable-next-line react/prefer-stateless-function
 export default class App extends Component {
   render() {
