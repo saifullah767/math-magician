ab668d96bdea978a51fcef2b94096f250b2ca4c1
skj99667788@gmail.com <skj99667788@gmail.com>
2022-05-25 03:08:40 +0500

    export change

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Calculator from './components/Calculator';
+import App from './App';
+// import Calculator from './components/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <App />
   </React.StrictMode>,
 );
 
