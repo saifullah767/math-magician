928c2b6e35753674c2add752681bd80bd7d3a65f
unknown <skj99667788@gmail.com>
2022-05-31 11:30:32 -0700

    first try

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import Calculator from './components/Calculator';
+import { Route, Routes } from 'react-router-dom';
+import CalculatorPage from './pages/CalculatorPage';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import QuotesPage from './pages/QuotesPage';
 import './App.css';
 
 /* eslint linebreak-style: ["error", "windows"] */
@@ -8,8 +12,14 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Calculator />
+        <Header />
+        <Routes>
+          <Route path="/math-magicians" element={<HomePage />} />
+          <Route path="/math-magicians/calculator" element={<CalculatorPage />} />
+          <Route path="/math-magicians/quotes" element={<QuotesPage />} />
+        </Routes>
       </div>
+      
     );
   }
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h2>Welcome to our page!</h2>
+      <p>
+        <b>Math Magicians Single Page Website is developed with React JS.</b>
+      </p>
+      <p>
+        Lorem ipsum dolor siye jo sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+        Fuga, sed labore odit voluptatem porro sunt saepe aliquid quaerat doloremque incidunt autem.
+
+      </p>
+      <p>
+        Lorem ipsum dolor siye jo sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+        Fuga, sed labore odit voluptatem porro sunt saepe aliquid quaerat doloremque incidunt autem.
+
+      </p>
+    </div>
+  );
+}
+export default Home;
